dc099ff4a232feacdcd9251895b15ca9509ea106
Pedro Henrique <122099407+xphzin-dzn@users.noreply.github.com>
2023-08-04 09:08:04 -0300

    Add files via upload

---
+++ TCA - PH/epinovo.js
@@ -0,0 +1,10 @@
+var modelsJson = [
+    {id:1, name:'Capacete Com Jugular', img:'capacete.jpg', price:[45.10, 80.00, 100.00], sizes:['P', 'M', 'G'], description:'PROTEÇÃO DA CABEÇA DO USUÁRIO CONTRA IMPACTOS DE OBJETOS SOBRE O CRÂNIO E CONTRA CHOQUES ELÉTRICOS.'},
+    {id:2, name:'Luva de Segurança (Trabalho em Altura)', img:'luva.webp', price:[60.00, 60.00, 60.00], sizes:['P', 'M', 'G'], description:'PROTEÇÃO DAS MÃOS DO USUÁRIO CONTRA AGENTES ABRASIVOS, ESCORIANTES, CORTANTES E PERFURANTES.'},
+    {id:3, name:'Botina com Biqueira de Composite', img:'botina.webp', price:[108.00, 108.00, 108.00], sizes:['39', '40', '41'], description:'PROTEÇÃO DOS PÉS DO USUÁRIO CONTRA IMPACTOS DE QUEDAS DE OBJETOS SOBRE OS ARTELHOS, CONTRA AGENTES ABRASIVOS E ESCORIANTES E CONTRA CHOQUES ELÉTRICOS.'},
+    {id:4, name:'Óculos de Segurança', img:'óculos.webp', price:[15.00, 15.00, 15.00], sizes:['P', 'M', 'G'], description:'PROTEÇÃO DOS OLHOS DO USUÁRIO CONTRA IMPACTOS DE PARTÍCULAS VOLANTES'},
+    {id:5, name:'Corda para trabalho em Altura', img:'corda.webp', price:[100.00, 200.00, 400.00], sizes:['3m', '6m', '12m'], description:'CORDA DE ALTA RESISTÊNCIA PARA TRABALHO EM ALTURA'},
+    {id:6, name:'Mosquetão', img:'mosquetão.webp', price:[10.00, 10.00, 10.00], sizes:['P', 'M', 'G'], description:'ANÉL METÁLICO COM UM SEGMENTO MÓVEL,DENOMINADO GATILHO,QUE SE ABRE PARA PERMITIR A PASSAGEM DA CORDA'},
+    {id:7, name:'Cinto de Segurança com Talabarte', img:'cinto.jfif', price:[200.00, 250.00, 299.99], sizes:['P', 'M', 'G'], description:'PROTEÇÃO DO USUÁRIO CONTRA RISCOS DE QUEDAS NOS TRABALHOS EM ALTURA'},
+    {id:8, name:'Trava-Quedas', img:'trava.webp', price:[570.00, 1600.00, 2300.00 ], sizes:['3m', '6m', '10m'], description:'UM DISPOSITIVO DE RETEÇÃO DE QUEDA O QUAL É LIGADO ENTRE O CINTURÃO DE SEGURANÇA E O PONTO DE ANCORAGEM'},
+];
